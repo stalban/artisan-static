5fadf436427fa70a2e9dbff3438283b14976bd27
Ranie Santos <raniesantos@users.noreply.github.com>
2020-04-29 22:22:07 +0800

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,6 +3,8 @@
 @section('content')
     <h1>Welcome to Artisan Static for Jigsaw</h1>
 
+    <h2>Update coming soon. See <a href="https://github.com/raniesantos/artisan-static-new">temporary repo</a>.</h2>
+
     <h2>Asset Compilation Test</h2>
 
     <p>CSS test: <span class="test-css">text with border and padding</span></p>
